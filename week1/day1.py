4bcede932e069fc02f35aa54495b7adef4f323a2
HunterMRocha <hmrocha2@dons.usfca.edu>
2020-09-02 18:47:06 -0700

    added code

---
+++ week1/day1.py
@@ -0,0 +1,62 @@
+#How to comment on your code
+
+#printing to the screen
+print("--Printing to the screen--")
+print("1. Hello World")
+print("2. Hunter Macias")
+print("3. This is one line \n4. This is another")
+print("--------------------------")
+
+#Creating different variables
+
+#Strings
+name = "Hunter"
+last_name = "Macias"
+work_email = "hunter@missionbit.org"
+slack_group = "???"
+
+#Ints
+age = 21
+health = 100
+tasks_remaining = 4
+items_available = 21
+
+#maybe go over floats too
+
+#printing strings and ints
+print()
+print("--Printing Strings and Ints--")
+print(name)
+print(last_name)
+print(age)
+print(health)
+print("-----------------------------")
+
+#Combining Strings and Ints
+print()
+print("--Combining strings and ints--")
+print("My name is", name, "\nI am", age, "years old")
+print("Player 2 has", health, "health")
+print("------------------------------")
+
+#Taking in string inputs
+print()
+print("--Taking in String Inputs--")
+name = input("Enter your name: ")
+last_name = input("Enter your last name: ")
+email = input("Enter your email: ")
+print()
+print("Personal Info:")
+print("\tFirst Name: ", name)
+print("\tLast Name: ", last_name)
+print("\tEmail: ", email)
+print("---------------------------")
+
+#taking in int input
+print()
+print("--Taking in Int Input--")
+age = int(input("enter your age: "))
+favorite_number = int(input("enter your fav number: "))
+
+
+#depending on time we might potentially go into day 2... 
---
+++ week1/day2.py
@@ -0,0 +1,37 @@
+from tabulate import tabulate
+
+
+savings_account = 2200
+weekly_pay = 1300
+monthly_pay = weekly_pay * 4
+annual_pay = monthly_pay * 12
+annual_expenses = 2200*12
+remaining_income = annual_pay - annual_expenses
+
+
+
+# We are going to try to simulate a savings account over the course of a year
+
+#print out all of our data with a table
+def printData():
+	print(' Savings  |  Weekly Income |  Monthly Income  |  Annual Income  |  Annual Expenses  |  Remaining Income')
+	print("--------------------------------------------------------------------------------------------------------")
+	print(f'  {savings_account}\t\t{weekly_pay}\t\t {monthly_pay}\t\t  {annual_pay}\t\t\t{annual_expenses}\t\t  {remaining_income}')
+
+
+print('Accounts at the end of one year')
+printData()
+print()
+print('Accounts at the end of two years')
+savings_account = savings_account + remaining_income
+weekly_pay = int(weekly_pay * 1.05)
+annual_expenses = int(annual_expenses * 1.1)
+printData()
+print()
+print('Accounts at the end of three years')
+savings_account = savings_account + remaining_income
+weekly_pay = int(weekly_pay * 1.06)
+annual_expenses = int(annual_expenses * 1.4)
+printData()
+
+
